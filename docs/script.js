211909c041dfff7d66fde2e35203f043c4613ed3
ununu <ununumio365@gmail.com>
2023-03-17 14:56:24 +0900

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -38,7 +38,7 @@ function handleDeviceOrientation(event) {
   const beta = event.beta / 90; // -1（下）〜1（上）
   const gamma = event.gamma / 90; // -1（左）〜1（右）
 
-  moveCharacter(gamma * stepSize, beta * stepSize);
+  moveCharacter(gamma * stepSize * 20, beta * stepSize * 20);
 }
 
 function handleKeyboardMove(e) {
@@ -67,61 +67,3 @@ window.addEventListener('deviceorientation', handleDeviceOrientation);
 
 // キーボード操作のイベントリスナー
 document.addEventListener('keydown', handleKeyboardMove);
-
-// ページがクリックされたら、DeviceOrientationEventの許可をリクエスト
-document.addEventListener('click', requestDeviceOrientationPermission);
-// 他のJavaScriptコード
-
-function resizeCanvas() {
-    const logoHeight = document.querySelector('.logo').offsetHeight;
-    const logoTop = document.querySelector('.logo').offsetTop;
-    canvas.width = window.innerWidth;
-    canvas.height = logoTop + logoHeight; // ロゴの下端まで描画されるように高さを調整
-}
-
-// 他のJavaScriptコード
-// ロゴがクリックされたときに画面をリセットする
-document.getElementById('reset-logo').addEventListener('click', () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    x = canvas.width / 2;
-    y = canvas.height / 2;
-    drawUnunN(x, y); // 「n」を中央に描画
-});
-
-// 他のJavaScriptコード
-
-// 方向定数
-const DIRECTION = {
-  UP: "UP",
-  DOWN: "DOWN",
-  LEFT: "LEFT",
-  RIGHT: "RIGHT",
-};
-
-// ランダムな方向を返す関数
-function randomDirection() {
-  const directions = Object.values(DIRECTION);
-  return directions[Math.floor(Math.random() * directions.length)];
-}
-
-// 0.1秒ごとにランダムな方向に動かす
-setInterval(() => {
-  const direction = randomDirection();
-
-  switch (direction) {
-    case DIRECTION.UP:
-      moveUnun(0, -1);
-      break;
-    case DIRECTION.DOWN:
-      moveUnun(0, 1);
-      break;
-    case DIRECTION.LEFT:
-      moveUnun(-1, 0);
-      break;
-    case DIRECTION.RIGHT:
-      moveUnun(1, 0);
-      break;
-  }
-}, 100);
-
-// 他のJavaScriptコード
